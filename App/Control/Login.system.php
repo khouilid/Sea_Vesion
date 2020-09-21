c198d536326720c411c0473f8018ede7fe771edf
khouilid <57531206+khouilid@users.noreply.github.com>
2020-09-21 15:33:38 +0100

    login system
    check if inputs empty

--- App/Control/Login.system.php
+++ App/Control/Login.system.php
@@ -3,18 +3,24 @@
 include '../Model/Login.system.php';
 
 
-$mail = $_POST['mail'];
-$password = $_POST['password'];
+if($_POST['mail'] != '' && $_POST['password'] != '' ){
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
+    //check if this accent exicte
+    $lets_log_in = new Log_in($mail, $password);
+    $result = $lets_log_in->log();
 
-
-//check if this accent exicte
-$lets_log_in = new Log_in($mail, $password);
-$result = $lets_log_in->log();
-
-if($result == 1){
-    header('location: ../View/Dashbord.php');
-    session_start();
-    $_SESSION['is_user'] = true;
+    if($result == 1){
+        header('location: ../View/Dashbord.php');
+        session_start();
+        $_SESSION['is_user'] = true;
+    }else{
+        header('location: ../../index.php?m=not_user ');
+    }
 }else{
-    header('location: ../../index.php?m=not_user ');
+    header('location: ../../index.php?m=empty ');
+
 }
+
+
+
